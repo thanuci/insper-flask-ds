f30725cf098cbf17fa766e274e13c398d5a2a0be
thanuci <thanuci@gmail.com>
2025-09-08 20:53:34 -0300

    Update de Thanuci main.py

--- main.py
+++ main.py
@@ -60,5 +60,5 @@ def sub(a, b):
     return str(float(a) - float(b))
     
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5027)))
 
